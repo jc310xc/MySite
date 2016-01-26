7f518588da5974c0420db05f49f6ff28a06cbb57
Peter Mullins <jc310xc@gmail.com>
2016-01-25 22:02:34 -0500

    Get canvas working

--- app/assets/javascripts/portfolio_controller.js
+++ app/assets/javascripts/portfolio_controller.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  var x = 150;
+  var y = 150;
+  var dx = 2;
+  var dy = -4;
+  var ctx;
+
+  function init() {
+    ctx = $('#mazeCanvas')[0].getContext("2d");
+    return setInterval(draw, 100);
+  }
+
+  function draw() {
+    ctx.clearRect(0,0,300,300);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+    x += dx;
+    y += dy;
+  }
+
+  init();
+});
