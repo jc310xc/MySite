b23914cd6d2379cd96eba2d2b236ac56a3ccb8ed
Peter Mullins <jc310xc@gmail.com>
2016-01-25 23:52:36 -0500

    Added some extra background options, changed drawing on canvas to something resembling the color palette I want

--- app/assets/javascripts/portfolio_controller.js
+++ app/assets/javascripts/portfolio_controller.js
@@ -9,17 +9,37 @@ $(document).ready(function(){
 
   function init() {
     ctx = $('#mazeCanvas')[0].getContext("2d");
-    return setInterval(draw, 100);
+    WIDTH = $("#mazeCanvas").width();
+    HEIGHT = $("#mazeCanvas").height();
+    return setTimeout(draw, 10);
+  }
+
+  function circle(x,y,r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function rect(x,y,w,h) {
+    ctx.beginPath();
+    ctx.rect(x,y,w,h);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
   }
 
   function draw() {
+    ctx.fillStyle("#BADA55");
     ctx.clearRect(0,0,300,300);
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
-    x += dx;
-    y += dy;
+    ctx.rect(100,100,10,10);
+    ctx.rect(115,115,10,10);
+    ctx.rect(130,130,10,10);
+    ctx.rect(145,145,10,10);
+    ctx.rect(160,160,10,10);
   }
 
   init();
