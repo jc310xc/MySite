12845754d32c585ebaa3d81b3d1b8cf631f6bbe8
Peter Mullins <jc310xc@gmail.com>
2016-03-02 07:52:41 -0500

    create Projects model and controller

--- db/migrate/20160302035926_create_projects.rb
+++ db/migrate/20160302035926_create_projects.rb
@@ -1,6 +1,10 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.text :technologies
+      t.binary :images
 
       t.timestamps null: false
     end
