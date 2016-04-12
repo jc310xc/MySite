f3d9e6ec12ca880507b9ea5e7c7b6dbeb2fa3bcb
Peter Mullins <jc310xc@gmail.com>
2016-04-12 08:51:31 -0400

    set up projects model/controller

--- db/migrate/20160306153308_create_projects.rb
+++ db/migrate/20160306153308_create_projects.rb
@@ -1,10 +1,11 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :title
       t.text :description
-      t.text :technologies
-      t.binary :images
+      t.string :role
+      t.string :tech
+      t.string :images
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121061452) do
+ActiveRecord::Schema.define(version: 20160306153308) do
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "role"
+    t.string   "tech"
+    t.string   "images"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
